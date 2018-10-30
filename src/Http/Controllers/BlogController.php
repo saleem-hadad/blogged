eb6c24560c77c9a8cf3fe0413e5ed1e541926494
Saleem Hadad <saleem_ha@hotmail.com>
2018-10-31 05:34:57 +0800

    wip

---
+++ src/Http/Controllers/BlogController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BinaryTorch\Blogged\Http\Controllers;
+
+class BlogController extends Controller
+{
+    /**
+     * Show the blog home page.
+     */
+    public function index()
+    {
+        return 'blog';
+    }
+}
