4bcda7f93e9e83d3b3c5887ced5e99b050be15e5
Saleem Hadad <saleem_ha@hotmail.com>
2018-10-31 20:34:33 +0800

    update UI

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -57,22 +57,27 @@
         <!-- Page content -->
         <section class="section">
             <div class="container">
-                    <div class="card shadow mt--100 no-border pb-2">
-                        <img class="card-img-top" src="https://i1.wp.com/wp.laravel-news.com/wp-content/uploads/2016/10/docker.png?resize=2200%2C1125" alt="Card image cap">
-                        <div class="bg-secondary px-5 py-2">
-                            <b>Published on:</b> <span class="description">OCTOBER 18, 2018</span>
-                            <button type="button" class="btn btn-outline-info btn-sm ml-2 pull-right">IIUM</button>
-                            <button type="button" class="btn btn-outline-danger btn-sm ml-2 pull-right">Education</button>
-                        </div>
-                        <div class="card-body px-5">
-                            <h1 class="display-1">How to use Argument Parser in Python</h1>
-                            <article class="pt-2">
-                                You most probably came across this issue when you deal with images using OpenCV or any other library, where you want to test your code on different images or input! 😕
-                                <hr>
-                                Usually template matching is done straight forward, but sometimes it needs to undergo several operations first such as conerting to grayscale, blurring and thresholding in some cases. Those operations are kind of crucial when programmers are looking for minimizing the processing time, because not all the time we need color and tiny details to be able to  match our template.
-                            </article>
-                        </div>
+                <div class="card shadow mt--100 no-border pb-2">
+                    <img class="card-img-top" src="https://i1.wp.com/wp.laravel-news.com/wp-content/uploads/2016/10/docker.png?resize=2200%2C1125" alt="Card image cap">
+                    <div class="bg-secondary px-5 py-2">
+                        <b>Published on:</b> <span class="description">OCTOBER 18, 2018</span>
+                        <button type="button" class="btn btn-outline-info btn-sm ml-2 pull-right">IIUM</button>
+                        <button type="button" class="btn btn-outline-danger btn-sm ml-2 pull-right">Education</button>
                     </div>
+                    <div class="card-body px-5">
+                        <h1 class="display-1">How to use Argument Parser in Python</h1>
+                        <article class="pt-2">
+                            You most probably came across this issue when you deal with images using OpenCV or any other library, where you want to test your code on different images or input! 😕
+                            <hr>
+                            Usually template matching is done straight forward, but sometimes it needs to undergo several operations first such as conerting to grayscale, blurring and thresholding in some cases. Those operations are kind of crucial when programmers are looking for minimizing the processing time, because not all the time we need color and tiny details to be able to  match our template.
+
+                            <blockquote class="alert alert-primary" role="alert">
+                                <span class="alert-inner--icon"><i class="ni ni-like-2"></i></span>
+                                <span class="alert-inner--text">LaRecipe will try to locate in each of your documentations versions in order to generate the sidebar index. You can create the files manually or you can generate them using LaRecipe docs generator.</span>
+                            </blockquote>
+                        </article>
+                    </div>
+                </div>
             </div>
         </section>
     </div>
