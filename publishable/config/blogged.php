05da851569ee5104b295d1fb3bd7c681b601655c
Saleem Hadad <saleem_ha@hotmail.com>
2018-11-01 05:35:19 +0800

    wip

---
+++ publishable/config/blogged.php
@@ -0,0 +1,116 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Documentation Routes
+    |--------------------------------------------------------------------------
+    |
+    | These options configure the behavior of the LaRecipe docs basic route
+    | where you can specify the url of your documentations, the location
+    | of your docs and the landing page when a user visits /docs route.
+    |
+    |
+    */
+
+    'routes' => [
+        'blog'  => 'blog',
+        'admin' => 'blog/admin',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Documentation Settings
+    |--------------------------------------------------------------------------
+    |
+    | These options configure the additional behaviors of your documentation
+    | where you can limit the access to only authenticated users in your
+    | system. It is false initially so that guests can view your docs.
+    |
+    | You may also specify links to show under the auth dropdown menu.
+    | Logout link will show by default.
+    |
+    |
+    */
+
+    'settings' => [
+        'ga_id' => ''
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Appearance
+    |--------------------------------------------------------------------------
+    |
+    | Here you can add configure the appearance of your docs. For example,
+    | you can swap the default logo to custom one that matches your Id
+    | Also, you can change the theme of your docs if you prefer that
+    |
+    | Supported Themes: 'light', 'dark'
+    |
+    */
+
+    'ui'                 => [
+        'show_app_name'  => true,
+        'logo'           => '', // e.g.: /images/logo.svg
+        'fav'            => '', // e.g.: /fav.png
+        'theme'          => 'light',
+        'code'           => 'dark',
+        'back_to_top'    => true,
+        'primary_color'  => '#787AF6',
+        'additional_css' => [
+            //'css/custom.css',
+        ],
+        'additional_js'  => [
+            //'js/custom.js',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | SEO
+    |--------------------------------------------------------------------------
+    |
+    | These options configure the SEO settings of your docs. You can set the
+    | author, the description and the keywords. Also, LaRecipe by default
+    | sets the canonical link to the viewed page's link automatically.
+    |
+    |
+    */
+
+    'seo'                 => [
+        'author'          => '',
+        'description'     => '',
+        'keywords'        => '',
+        'og'              => [
+            'title'       => '',
+            'type'        => 'article',
+            'url'         => '',
+            'image'       => '',
+            'description' => '',
+        ]
+    ],
+
+   /*
+   |--------------------------------------------------------------------------
+   | Forum
+   |--------------------------------------------------------------------------
+   |
+   | Giving a chance to your users to post their questions or feedback
+   | directly on your docs, is pretty nice way to engage them more.
+   | However, you can also enable/disable the forum's visibility.
+   |
+   | Supported Services: 'disqus'
+   |
+   */
+
+  'forum'                 => [
+        'enabled'           => false,
+        'default'           => 'disqus',
+        'services'          => [
+            'disqus'        => [
+                'site_name' => '', // yoursite.disqus.com
+            ]
+        ]
+    ]
+];
