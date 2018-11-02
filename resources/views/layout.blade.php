a3359b094d89add083bad7061d9f72e5e5f165c6
Saleem Hadad <saleem_ha@hotmail.com>
2018-11-03 06:26:38 +0800

    update css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,18 +13,19 @@
         {{-- CSS --}}
         <link rel="stylesheet" href="{{ blogged_assets('css/app.css') }}">
 
-        <style>
-            .row.is-flex {
-                display: flex;
-                flex-wrap: wrap;
-            }
-            .row.is-flex > [class*='col-'] {
-                display: flex;
-            }
-        </style>
+        {{-- Icon --}}
+        <link rel="apple-touch-icon" href="{{ asset(config('blogged.ui.fav')) }}">
+        <link rel="shortcut icon" type="image/png" href="{{ asset(config('blogged.ui.fav')) }}"/>
 
         {{-- JS --}}
         <script src="{{ blogged_assets('js/app.js') }}" defer></script>
+
+        {{-- Custom CSS --}}
+        @if(!empty(config('blogged.ui.additional_css')))
+            @foreach(config('blogged.ui.additional_css') as $css)
+                <link rel="stylesheet" type="text/css" href="{{ asset($css) }}">
+            @endforeach
+        @endif
     </head>
     <body>
         <div id="app">
@@ -36,5 +37,12 @@
                 <back-to-top></back-to-top>
             @endif
         </div>
+
+        {{-- Custom JS --}}
+        @if(!empty(config('blogged.ui.additional_js')))
+            @foreach(config('blogged.ui.additional_js') as $js)
+                <script type="text/javascript" src="{{ asset($js) }}"></script>
+            @endforeach
+        @endif
     </body>
 </html>
