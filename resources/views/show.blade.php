4163b1e7cc84ec5986454ee472417a3067735487
Saleem Hadad <saleem_ha@hotmail.com>
2018-11-03 04:45:58 +0800

    wip

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -10,7 +10,7 @@
 
         <!-- Page content -->
         <section class="section">
-            <div class="container" style="margin-top: -100px">
+            <div class="container" style="margin-top: -100px; margin-bottom: 100px">
                 <div class="card shadow no-border pb-2">
                     <img class="card-img-top" src="https://i1.wp.com/wp.laravel-news.com/wp-content/uploads/2016/10/docker.png?resize=2200%2C1125" alt="Card image cap">
                     <div class="bg-secondary px-5 py-2">
