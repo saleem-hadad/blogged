e4600d57f4fe420fee54c923a59bfe9a9667c408
Saleem Hadad <saleem_ha@hotmail.com>
2018-11-03 12:47:31 +0800

    wip

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'ArticleController@index')->name('index');
 
 // Dashboard routes..
-Route::get(config('blogged.routes.dashboard'), 'DashboardController@index')->middleware('blogged')->name('dashboard.index');
 Route::get(config('blogged.routes.login'), 'DashboardController@index')->name('login');
+Route::get(config('blogged.routes.dashboard'), 'DashboardController@index')->middleware('blogged')->name('dashboard.index');
 
 // Dynamic model matching..
 Route::get('/{article}', 'ArticleController@show')->name('show');
