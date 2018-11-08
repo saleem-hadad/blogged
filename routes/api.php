f11c2a6dea30643041ea22c787c4f165f3f4ee36
Saleem Hadad <saleem_ha@hotmail.com>
2018-11-08 20:42:48 +0800

    update api routes

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Blogged Articles
 Route::get('/articles', 'ArticleController@index');
 Route::post('/articles', 'ArticleController@store');
+Route::get('/articles/{article}', 'ArticleController@show');
+Route::put('/articles/{article}', 'ArticleController@update');
+Route::delete('/articles/{article}', 'ArticleController@delete');
+
+// Blogged Categories...
+Route::get('/categories', 'CategoryController@index');
+Route::post('/categories', 'CategoryController@store');
+Route::put('/categories/{category}', 'CategoryController@update');
+Route::delete('/categories/{category}', 'CategoryController@delete');
+
+// Blogged Tags...
+Route::get('/tags', 'TagController@index');
+Route::post('/tags', 'TagController@store');
+Route::put('/tags/{tag}', 'TagController@update');
+Route::delete('/tags/{tag}', 'TagController@delete');
+
+// Blog Files Uploads..
+Route::post('/files', 'FileController@store');
