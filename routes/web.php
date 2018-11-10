40d6a95ce34223349e16d5ca74e545488f36163c
Saleem Hadad <saleem_ha@hotmail.com>
2018-11-10 21:42:37 +0800

    update dashboard spa route

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Dashboard route..
-Route::view(config('blogged.routes.dashboard'), 'blogged::dashboard.index')->middleware('blogged');
-Route::view(config('blogged.routes.dashboard') . '/{page}', 'blogged::dashboard.index')->where('page', '.*')->middleware('blogged');
+// Dashboard route (SPA)..
+Route::view(config('blogged.routes.dashboard') . '/{page?}', 'blogged::dashboard.index')->where('page', '.*')->middleware('blogged');
 
 // Blog routes..
 Route::get(config('blogged.routes.blog') . '/', 'BlogController@index')->name('index');
